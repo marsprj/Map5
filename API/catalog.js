dfc3e4498e2d8f70548ee0a2b47b8d2ad7d21752
fanzia <caisefan@163.com>
2016-07-04 14:01:59 +0800

    修改代码，定位Map5为一个纯客户端。增加API页面。

---
+++ API/catalog.js
@@ -0,0 +1,143 @@
+var g_catalog = [
+	{
+		"name": "地图基本操作",
+		"items":[
+			{
+				"name" : "初始化地图",
+				"link" : "initMap.html"
+			},{
+				"name" : "关闭地图",
+				"link" : "closeMap.html"
+			},{
+				"name" : "拖拽地图",
+				"link" : "dragMap.html"
+			},{
+				"name" : "缩放地图",
+				"link" : "scrollMap.html"
+			},{
+				"name" : "拉框缩放",
+				"link" : "zoomMap.html"
+			}
+		]
+	},{
+		"name" : "地图状态控制",
+		"items":[
+			{
+				"name" : "地图级别",
+				"link" : "mapLevel.html"
+			},{
+				"name" : "地图中心点",
+				"link" : "mapCenter.html"
+			},{
+				"name" : "导航条",
+				"link" : "mapNav.html"
+			}
+		]
+	},{
+		"name" : "图层控制",
+		"items":[
+			{
+				"name" : "添加图层",
+				"link" : "addLayer.html"
+			},{
+				"name" : "删除图层",
+				"link" : "removeLayer.html"
+			},{
+				"name" : "图层显示",
+				"link" : "layerVisible.html"
+			},{
+				"name" : "QuadServer图层",
+				"link" : "QSLayer.html"
+			},{
+				"name" : "WFS图层",
+				"link" : "WFSLayer.html"
+			},{
+				"name" : "WMS图层",
+				"link" : "WMSLayer.html"
+			}
+		]
+	},{
+		"name" : "样式设置",
+		"items":[
+			{
+				"name" : "点样式设置",
+				"link" : "pointStyle.html"
+			},{
+				"name" : "线样式设置",
+				"link" : "lineStyle.html"
+			},{
+				"name" : "面样式设置",
+				"link" : "polygonStyle.html"
+			},{
+				"name" : "唯一值样式设置",
+				"link" : "uniqueStyle.html"
+			},{
+				"name" : "分级样式设置",
+				"link" : ""
+			}
+		]
+
+	// },{
+	// 	"name" : "专题图",
+	// 	"items":[
+	// 		{
+	// 			"name" : "柱状图",
+	// 			"link" : ""
+	// 		},{
+	// 			"name" : "饼状图",
+	// 			"link" : ""
+	// 		},{
+	// 			"name" : "热力图",
+	// 			"link" : ""
+	// 		},{
+	// 			"name" : "点聚合图",
+	// 			"link" : ""
+	// 		}
+	// 	]
+	},{
+		"name" : "WFS查询",
+		"items":[
+			{
+				"name" : "缓冲区查询",
+				"link" : "wfsBuffer.html"
+			},{
+				"name" : "拉框查询",
+				"link" : "wfsQueryRect.html"
+			// },{
+			// 	"name" : "hit查询",
+			// 	"link" : ""
+			}
+		]
+	},{
+		"name" : "标注",
+		"items":[
+			{
+				"name" : "marker标注",
+				"link" : "marker.html"
+			},{
+				"name" : "线标注",
+				"link" : "polyline.html"
+			},{
+				"name" : "面标注",
+				"link" : "polygon.html"
+			},{
+				"name" : "infoWindow标注",
+				"link" : "infoWindow.html"
+			}
+		]
+	},{
+		"name" : "事件",
+		"items":[
+			{
+				"name" : "鼠标移动事件",
+				"link" : "event.html"
+			},{
+				"name" : "拖拽事件",
+				"link" : "dragEvent.html"
+			},{
+				"name" : "鼠标滚动事件",
+				"link" : "wheelEvent.html"
+			}
+		]
+	}
+];
