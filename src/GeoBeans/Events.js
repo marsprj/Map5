1880acb9152767a444d83821d15bfed2d2d8a7ef
marsprj <marsprj@gmail.com>
2016-09-09 17:02:47 +0800

    修改Events.js，对events的管理采用hashmap的方式

--- src/GeoBeans/Events.js
+++ src/GeoBeans/Events.js
@@ -1,61 +1,78 @@
-GeoBeans.Event = {	
-	/* mouse event */
-	CLICK 		: "click",
+GeoBeans.Event.EventType = {
+	/* Map event */
+	CLICK 		: "click",			
 	DCLICK 		: "dblclick",
+	RESIZE		: "resize",
+
+	CHANGE		: "change",
+	DRAG_BEGIN	: "dragbegin",
+	DRAGING		: "dragging",
+	DRAG_END	: "dragend",
+
+	/* mouse Event */
 	MOUSE_DOWN	: "mousedown",
 	MOUSE_UP 	: "mouseup",
 	MOUSE_MOVE 	: "mousemove",
 	MOUSE_OVER 	: "mouseover",
 	MOUSE_OUT 	: "mouseout",
-	RESIZE		: "resize",
 	MOUSE_WHEEL : "mousewheel"
-};
 
-/*
- * 鼠标按键
- */
-GeoBeans.Event.MouseButton = {
-	LEFT	: "left",
-	RIGHT	: "right",
-	MID		: "mid"
-};
-
-/*
- * 鼠标事件
- */
-GeoBeans.Event.MouseArgs = function(){
-	buttn : null;
-	X : null;
-	Y : null;
-	mapX : null;
-	mapY : null;
-	level : null;
+	/* touch Event */
+	TOUCHSTART	: 'touchstart',
+	TOUCHMOVE 	: 'touchmove',
+	TOUCHEND 	: 'touchend',
+	WHEEL 		: 'wheel'
 };
 
 GeoBeans.Events = GeoBeans.Class({
 	
-	events : null,
+	_events : {},
 	
 	initialize : function(){
-		this.events = [];
 	},
 	
-	destory : function(){
-		this.events = null;
-	},
-	
-	addEvent : function(evt, evt_handler){
-		this.events.push({event : evt, handler : evt_handler});
-	},
-	
-	getEvnet : function(event){
-		var len = this.events;
-		for(var i=0; i<len; i++){
-			var evt = this.events[i];
-			if(evt.event == event){
-				return evt;
-			}
-		}
-		return null;
-	}
+	on : null,
+	un : null,
+	get: null
+
 });
+
+/**
+ * [destory description]
+ * @return {[type]} [description]
+ */
+GeoBeans.Events.prototype.destory = function(){
+		this._events = null;
+}
+
+/**
+ * [on description]
+ * @param  {[type]} evt         [description]
+ * @param  {[type]} evt_handler [description]
+ * @return {[type]}             [description]
+ */
+GeoBeans.Events.prototype.on = function(evt, evt_handler){
+	this._events[evt] = evt_handler;
+}
+
+/**
+ * [un description]
+ * @param  {[type]} evt [description]
+ * @return {[type]}     [description]
+ */
+GeoBeans.Events.prototype.un = function(evt){
+	handler = this._events[evt];
+	if(handler!=undefined){
+		//this._events[evt] = undefined;	
+		delete this._events[evt];
+	}
+}
+	
+/**
+ * 
+ * @param  {[type]} evt [description]
+ * @return {[type]}     [description]
+ */
+GeoBeans.Events.prototype.get = function(evt){		
+	return this._events[evt];
+}
