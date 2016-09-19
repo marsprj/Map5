412b5fba152de677ba461149607ffc930d989bdc
marsprj <marsprj@gmail.com>
2016-09-19 14:17:51 +0800

    增加example/all下面的示例

---
+++ example/all/js/catalog.js
@@ -0,0 +1,318 @@
+var g_catalog = [
+	{
+		"name": "地图基本操作",
+		"items":[
+			{
+				"name" : "初始化地图",
+				"link" : "map/map_init.html"
+			},{
+				"name" : "关闭地图",
+				"link" : "map/map_close.html"
+			},{
+				"name" : "拖拽地图",
+				"link" : "map/map_drag.html"
+			},{
+				"name" : "滚轮缩放",
+				"link" : "map/map_scroll.html"
+			},{
+				"name" : "拉框缩放",
+				"link" : "map/map_zoom.html"
+			}
+		]
+	},{
+		"name" : "地图状态控制",
+		"items":[
+			{
+				"name" : "地图级别",
+				"link" : "map/map_view_zoom.html"
+			},{
+				"name" : "地图中心点",
+				"link" : "map_set_center.html"
+			},{
+				"name" : "地图范围",
+				"link" : "map_set_extent.html"
+			},{
+				"name" : "导航条",
+				"link" : "map_widget_nav.html"
+			// },{
+			// 	"name" : "地图旋转",
+			// 	"link" : "rotate.html"
+			}
+		]
+	},{
+		"name" : "图层控制",
+		"items":[
+			{
+				"name" : "添加图层",
+				"link" : "layer_add.html"
+			},{
+				"name" : "删除图层",
+				"link" : "layer_remove.html"
+			},{
+				"name" : "图层显示",
+				"link" : "layer_visible.html"
+			},{
+				"name" : "QuadServer图层",
+				"link" : "layer_add_quadserver.html"
+			},{
+				"name" : "多个QuadServer图层",
+				"link" : "layer_add_multi_quadserver.html"
+			},{
+				"name" : "PGIS图层",
+				"link" : "layer_add_pgis.html"
+			},{
+				"name" : "WFS图层",
+				"link" : "layer_add_wfs.html"
+			},{
+				"name" : "WMS图层",
+				"link" : "layer_add_wms.html"
+			}
+		]
+	},{
+		"name" : "地图交互",
+		"items":[
+			{
+				"name" : "点击查询",
+				"link" : "interaction/interaction_select_by_click.html"
+			},{
+				"name" : "矩形查询",
+				"link" : "interaction/interaction_select_by_rect.html"
+			},{
+				"name" : "圆查询",
+				"link" : "interaction/interaction_select_by_circle.html"
+			},{
+				"name" : "线查询",
+				"link" : "interaction/interaction_select_by_line.html"
+			},{
+				"name" : "多边形查询",
+				"link" : "interaction/interaction_select_by_polygon.html"
+			},{
+				"name" : "地图旋转",
+				"link" : "layer_remove.html"
+			}
+		]
+	},{
+		"name" : "要素图层",
+		"items":[
+			{
+				"name" : "点要素图层",
+				"link" : "pointFeatureLayer.html"
+			},{
+				"name" : "多点要素图层",
+				"link" : "multiPointFeatureLayer.html"
+			},{
+				"name" : "线要素图层",
+				"link" : "lineFeatureLayer.html"
+			},{
+				"name" : "多线要素图层",
+				"link" : "multiLineStringFeatureLayer.html"
+			},{
+				"name" : "面要素图层",
+				"link" : "polygonFeatureLayer.html"
+			},{
+				"name" : "多面要素图层",
+				"link" : "multiPolygonFeatureLayer.html"
+			}
+		]
+	},{
+		"name" : "样式设置",
+		"items":[
+			{
+				"name" : "点样式设置",
+				"link" : "pointStyle.html"
+			},{
+				"name" : "线样式设置",
+				"link" : "lineStyle.html"
+			},{
+				"name" : "面样式设置",
+				"link" : "polygonStyle.html"
+			},{
+				"name" : "点图层文字样式",
+				"link" : "pointStyle_text.html"
+			},{
+				"name" : "线图层文字样式",
+				"link" : "lineStyle_text.html"
+			},{
+				"name" : "面图层文字样式",
+				"link" : "polygonStyle_text.html"
+			},{
+				"name" : "唯一值样式设置",
+				"link" : "uniqueStyle.html"
+			},{
+				"name" : "分级样式设置",
+				"link" : "classStyle.html"
+			}
+		]
+	},{
+		"name" : "专题图",
+		"items":[
+			{
+				"name" : "柱状图",
+				"link" : "barChart.html"
+			},{
+				"name" : "饼状图",
+				"link" : "pieChart.html"
+			},{
+				"name" : "等级符号图",
+				"link" : "symbolChart.html"
+			},{
+				"name" : "热力图",
+				"link" : "heatMap.html"
+			},{
+				"name" : "点聚合图-示例1",
+				"link" : "clusterLayer.html"
+			},{
+				"name" : "点聚合图-示例2",
+				"link" : "clusterLayer1.html"
+			},{
+				"name" : "点聚合图-示例3",
+				"link" : "clusterLayer2.html"
+			},{
+				"name" : "分级图",
+				"link" : "rangeChart.html"
+			}
+		]
+	},{
+		"name" : "WFS查询",
+		"items":[
+			{
+				"name" : "点击查询",
+				"link" : "layerClickEvent.html"
+			},{
+				"name" : "缓冲区查询",
+				"link" : "wfsBuffer.html"
+			},{
+				"name" : "拉框查询",
+				"link" : "wfsQueryRect.html"
+			},{
+				"name" : "属性查询",
+				"link" : "ComparisionFilterQuery.html"
+			},{
+				"name" : "空间查询",
+				"link" : "spatialFilterQuery.html"
+			},{
+				"name" : "逻辑查询",
+				"link" : "logicFilterQuery.html"
+			}
+		]
+	},{
+		"name" : "要素图层查询",
+		"items":[
+			{
+				"name" : "点击查询",
+				"link" : "featureLayerClickEvent.html"
+			},{
+				"name" : "拉框查询",
+				"link" : "featureLayerQueryRect.html"
+			},{
+				"name" : "属性查询",
+				"link" : "featureLayerComparisionFilterQuery.html"
+			}
+		]		
+	},{
+		"name" : "标注",
+		"items":[
+			{
+				"name" : "marker标注",
+				"link" : "marker.html"
+			},{
+				"name" : "线标注",
+				"link" : "polyline.html"
+			},{
+				"name" : "面标注",
+				"link" : "polygon.html"
+			},{
+				"name" : "文字标注",
+				"link" : "label.html"
+			},{
+				"name" : "infoWindow标注",
+				"link" : "infoWindow.html"
+			},{
+				"name" : "标注操作",
+				"link" : "overlay.html"
+			}
+		]
+	},{
+		"name" : "标绘",
+		"items":[
+			{
+				"name" : "点线面标绘",
+				"link" : "drawOverlay.html"
+			},{
+				"name" : "标绘&标注点击事件",
+				"link" : "overlayClick.html"
+			}
+		]
+	},{
+		"name" : "事件",
+		"items":[
+			{
+				"name" : "鼠标移动事件",
+				"link" : "event.html"
+			},{
+				"name" : "拖拽事件",
+				"link" : "dragEvent.html"
+			},{
+				"name" : "鼠标滚动事件",
+				"link" : "wheelEvent.html"
+			}
+		]
+	},{
+		"name" : "加载KML文件",
+		"items":[
+			{
+				"name" : "点",
+				"link" : "kml_point.html"
+			},{
+				"name" : "线",
+				"link" : "kml_line.html"
+			},{
+				"name" : "面",
+				"link" : "kml_polygon.html"
+			},{
+				"name" : "例子1",
+				"link" : "kml_example_1.html"
+			},{
+				"name" : "例子2",
+				"link" : "kml_example_2.html"
+			},{
+				"name" : "例子3",
+				"link" : "kml_example_3.html"
+			}
+		]
+	},{
+		"name" : "加载GeoJson文件",
+		"items":[
+			{
+				"name" : "点",
+				"link" : "geojson_point.html"
+			},{
+				"name" : "线",
+				"link" : "geojson_line.html"
+			},{
+				"name" : "面",
+				"link" : "geojson_polygon.html"
+			},{
+				"name" : "例子1",
+				"link" : "geojson_example_1.html"
+			},{
+				"name" : "例子2",
+				"link" : "geojson_example_2.html"
+			},{
+				"name" : "例子3",
+				"link" : "geojson_example_3.html"
+			}
+		]		
+	},{
+		"name" : "动画",
+		"items":[
+			{
+				"name" : "动画轨迹（未全部实现）",
+				"link" : "animation.html"
+			},{
+				"name" : "动态迁徙图",
+				"link" : "geoLineLayer.html"
+			}
+		]
+	}
+];
