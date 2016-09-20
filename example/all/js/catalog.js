bb05dcad71a35772045471eb6b8663f50cb8bd51
renyc <renyc@radi.ac.cn>
2016-09-20 08:49:04 +0800

    调整example的catalog.js

--- example/all/js/catalog.js
+++ example/all/js/catalog.js
@@ -1,12 +1,12 @@
 var g_catalog = [
 	{
-		"name": "地图基本操作",
+		"name": "地图",
 		"items":[
 			{
 				"name" : "初始化地图",
 				"link" : "map/map_init.html"
 			},{
-				"name" : "关闭地图",
+				"name" : "(*)关闭地图",
 				"link" : "map/map_close.html"
 			},{
 				"name" : "拖拽地图",
@@ -26,13 +26,13 @@ var g_catalog = [
 				"name" : "导航条",
 				"link" : "control/control_nav.html"
 			},{
-				"name" : "鹰眼（待实现）",
+				"name" : "(*)鹰眼（待实现）",
 				"link" : "control/control_hawk.html"
 			},{
-				"name" : "比例尺（待实现）",
+				"name" : "(*)比例尺（待实现）",
 				"link" : "control/control_scalebar.html"
 			},{
-				"name" : "图例（待补充）",
+				"name" : "(*)图例（待补充）",
 				"link" : "control/control_legend.html"
 			},{
 				"name" : "版权（待补充）",
@@ -58,7 +58,7 @@ var g_catalog = [
 				"name" : "导航条",
 				"link" : "map_widget_nav.html"
 			// },{
-			// 	"name" : "地图旋转",
+			// 	"name" : "(*)地图旋转",
 			// 	"link" : "rotate.html"
 			}
 		]
@@ -75,31 +75,31 @@ var g_catalog = [
 				"name" : "WMS图层",
 				"link" : "layer/layer_add_wms.html"
 			},{
-				"name" : "WMTS图层",
+				"name" : "(*)WMTS图层",
 				"link" : "layer/layer_add_wmts.html"
 			},{
-				"name" : "QuadServer图层",
+				"name" : "(*)QuadServer图层",
 				"link" : "layer/layer_add_quadserver.html"
 			},{
-				"name" : "多个QuadServer图层",
+				"name" : "(*)多个QuadServer图层",
 				"link" : "layer/layer_add_multi_quadserver.html"
 			},{
-				"name" : "PGIS图层",
+				"name" : "(*)PGIS图层",
 				"link" : "layer/layer_add_pgis.html"
 			},{
-				"name" : "高德图层",
+				"name" : "(*)高德图层",
 				"link" : "layer/layer_add_gaode.html"
 			},{
-				"name" : "百度图层",
+				"name" : "(*)百度图层",
 				"link" : "layer/layer_add_baidu.html"
 			},{
-				"name" : "天地图层",
+				"name" : "(*)天地图层",
 				"link" : "layer/layer_add_tianditu.html"
 			},{
-				"name" : "OSM图层(待实现)",
+				"name" : "(*)OSM图层(待实现)",
 				"link" : "layer/layer_add_osm.html"
 			},{
-				"name" : "Bing图层(待实现)",
+				"name" : "(*)Bing图层(待实现)",
 				"link" : "layer/layer_add_bing.html"
 			}
 		]
@@ -107,10 +107,10 @@ var g_catalog = [
 		"name" : "图层控制",
 		"items":[
 			{
-				"name" : "删除图层",
+				"name" : "(*)删除图层",
 				"link" : "layer/layer_remove.html"
 			},{
-				"name" : "图层显示",
+				"name" : "(*)图层显示",
 				"link" : "layer/layer_visible.html"
 			}
 		]
@@ -127,13 +127,13 @@ var g_catalog = [
 				"name" : "查询(圆)",
 				"link" : "interaction/interaction_select_by_circle.html"
 			},{
-				"name" : "查询(线)",
+				"name" : "(*)查询(线)",
 				"link" : "interaction/interaction_select_by_line.html"
 			},{
-				"name" : "查询(多边形)",
+				"name" : "(*)查询(多边形)",
 				"link" : "interaction/interaction_select_by_polygon.html"
 			},{
-				"name" : "绘制图元",
+				"name" : "(*)绘制图元",
 				"link" : "interaction/interaction_draw.html"
 			},{
 				"name" : "地图旋转",
@@ -144,22 +144,22 @@ var g_catalog = [
 		"name" : "要素图层",
 		"items":[
 			{
-				"name" : "点要素图层",
+				"name" : "(*)点要素图层",
 				"link" : "pointFeatureLayer.html"
 			},{
-				"name" : "多点要素图层",
+				"name" : "(*)多点要素图层",
 				"link" : "multiPointFeatureLayer.html"
 			},{
-				"name" : "线要素图层",
+				"name" : "(*)线要素图层",
 				"link" : "lineFeatureLayer.html"
 			},{
-				"name" : "多线要素图层",
+				"name" : "(*)多线要素图层",
 				"link" : "multiLineStringFeatureLayer.html"
 			},{
-				"name" : "面要素图层",
+				"name" : "(*)面要素图层",
 				"link" : "polygonFeatureLayer.html"
 			},{
-				"name" : "多面要素图层",
+				"name" : "(*)多面要素图层",
 				"link" : "multiPolygonFeatureLayer.html"
 			}
 		]
@@ -167,28 +167,28 @@ var g_catalog = [
 		"name" : "样式设置",
 		"items":[
 			{
-				"name" : "点样式设置",
+				"name" : "(*)点样式设置",
 				"link" : "pointStyle.html"
 			},{
-				"name" : "线样式设置",
+				"name" : "(*)线样式设置",
 				"link" : "lineStyle.html"
 			},{
-				"name" : "面样式设置",
+				"name" : "(*)面样式设置",
 				"link" : "polygonStyle.html"
 			},{
-				"name" : "点图层文字样式",
+				"name" : "(*)点图层文字样式",
 				"link" : "pointStyle_text.html"
 			},{
-				"name" : "线图层文字样式",
+				"name" : "(*)线图层文字样式",
 				"link" : "lineStyle_text.html"
 			},{
-				"name" : "面图层文字样式",
+				"name" : "(*)面图层文字样式",
 				"link" : "polygonStyle_text.html"
 			},{
-				"name" : "唯一值样式设置",
+				"name" : "(*)唯一值样式设置",
 				"link" : "uniqueStyle.html"
 			},{
-				"name" : "分级样式设置",
+				"name" : "(*)分级样式设置",
 				"link" : "classStyle.html"
 			}
 		]
@@ -196,28 +196,28 @@ var g_catalog = [
 		"name" : "专题图",
 		"items":[
 			{
-				"name" : "柱状图",
+				"name" : "(*)柱状图",
 				"link" : "barChart.html"
 			},{
-				"name" : "饼状图",
+				"name" : "(*)饼状图",
 				"link" : "pieChart.html"
 			},{
-				"name" : "等级符号图",
+				"name" : "(*)等级符号图",
 				"link" : "symbolChart.html"
 			},{
-				"name" : "热力图",
+				"name" : "(*)热力图",
 				"link" : "heatMap.html"
 			},{
-				"name" : "点聚合图-示例1",
+				"name" : "(*)点聚合图-示例1",
 				"link" : "clusterLayer.html"
 			},{
-				"name" : "点聚合图-示例2",
+				"name" : "(*)点聚合图-示例2",
 				"link" : "clusterLayer1.html"
 			},{
-				"name" : "点聚合图-示例3",
+				"name" : "(*)点聚合图-示例3",
 				"link" : "clusterLayer2.html"
 			},{
-				"name" : "分级图",
+				"name" : "(*)分级图",
 				"link" : "rangeChart.html"
 			}
 		]
@@ -225,22 +225,22 @@ var g_catalog = [
 		"name" : "WFS查询",
 		"items":[
 			{
-				"name" : "点击查询",
+				"name" : "(*)点击查询",
 				"link" : "layerClickEvent.html"
 			},{
-				"name" : "缓冲区查询",
+				"name" : "(*)缓冲区查询",
 				"link" : "wfsBuffer.html"
 			},{
-				"name" : "拉框查询",
+				"name" : "(*)拉框查询",
 				"link" : "wfsQueryRect.html"
 			},{
-				"name" : "属性查询",
+				"name" : "(*)属性查询",
 				"link" : "ComparisionFilterQuery.html"
 			},{
-				"name" : "空间查询",
+				"name" : "(*)空间查询",
 				"link" : "spatialFilterQuery.html"
 			},{
-				"name" : "逻辑查询",
+				"name" : "(*)逻辑查询",
 				"link" : "logicFilterQuery.html"
 			}
 		]
@@ -248,13 +248,13 @@ var g_catalog = [
 		"name" : "要素图层查询",
 		"items":[
 			{
-				"name" : "点击查询",
+				"name" : "(*)点击查询",
 				"link" : "featureLayerClickEvent.html"
 			},{
-				"name" : "拉框查询",
+				"name" : "(*)拉框查询",
 				"link" : "featureLayerQueryRect.html"
 			},{
-				"name" : "属性查询",
+				"name" : "(*)属性查询",
 				"link" : "featureLayerComparisionFilterQuery.html"
 			}
 		]		
@@ -262,22 +262,22 @@ var g_catalog = [
 		"name" : "标注",
 		"items":[
 			{
-				"name" : "marker标注",
+				"name" : "(*)marker标注",
 				"link" : "marker.html"
 			},{
-				"name" : "线标注",
+				"name" : "(*)线标注",
 				"link" : "polyline.html"
 			},{
-				"name" : "面标注",
+				"name" : "(*)面标注",
 				"link" : "polygon.html"
 			},{
-				"name" : "文字标注",
+				"name" : "(*)文字标注",
 				"link" : "label.html"
 			},{
-				"name" : "infoWindow标注",
+				"name" : "(*)infoWindow标注",
 				"link" : "infoWindow.html"
 			},{
-				"name" : "标注操作",
+				"name" : "(*)标注操作",
 				"link" : "overlay.html"
 			}
 		]
@@ -285,10 +285,10 @@ var g_catalog = [
 		"name" : "标绘",
 		"items":[
 			{
-				"name" : "点线面标绘",
+				"name" : "(*)点线面标绘",
 				"link" : "drawOverlay.html"
 			},{
-				"name" : "标绘&标注点击事件",
+				"name" : "(*)标绘&标注点击事件",
 				"link" : "overlayClick.html"
 			}
 		]
@@ -296,13 +296,13 @@ var g_catalog = [
 		"name" : "事件",
 		"items":[
 			{
-				"name" : "鼠标移动事件",
+				"name" : "(*)鼠标移动事件",
 				"link" : "event.html"
 			},{
-				"name" : "拖拽事件",
+				"name" : "(*)拖拽事件",
 				"link" : "dragEvent.html"
 			},{
-				"name" : "鼠标滚动事件",
+				"name" : "(*)鼠标滚动事件",
 				"link" : "wheelEvent.html"
 			}
 		]
@@ -310,22 +310,22 @@ var g_catalog = [
 		"name" : "加载KML文件",
 		"items":[
 			{
-				"name" : "点",
+				"name" : "(*)点",
 				"link" : "kml_point.html"
 			},{
-				"name" : "线",
+				"name" : "(*)线",
 				"link" : "kml_line.html"
 			},{
-				"name" : "面",
+				"name" : "(*)面",
 				"link" : "kml_polygon.html"
 			},{
-				"name" : "例子1",
+				"name" : "(*)例子1",
 				"link" : "kml_example_1.html"
 			},{
-				"name" : "例子2",
+				"name" : "(*)例子2",
 				"link" : "kml_example_2.html"
 			},{
-				"name" : "例子3",
+				"name" : "(*)例子3",
 				"link" : "kml_example_3.html"
 			}
 		]
@@ -333,22 +333,22 @@ var g_catalog = [
 		"name" : "加载GeoJson文件",
 		"items":[
 			{
-				"name" : "点",
+				"name" : "(*)点",
 				"link" : "geojson_point.html"
 			},{
-				"name" : "线",
+				"name" : "(*)线",
 				"link" : "geojson_line.html"
 			},{
-				"name" : "面",
+				"name" : "(*)面",
 				"link" : "geojson_polygon.html"
 			},{
-				"name" : "例子1",
+				"name" : "(*)例子1",
 				"link" : "geojson_example_1.html"
 			},{
-				"name" : "例子2",
+				"name" : "(*)例子2",
 				"link" : "geojson_example_2.html"
 			},{
-				"name" : "例子3",
+				"name" : "(*)例子3",
 				"link" : "geojson_example_3.html"
 			}
 		]		
@@ -356,10 +356,10 @@ var g_catalog = [
 		"name" : "动画",
 		"items":[
 			{
-				"name" : "动画轨迹（未全部实现）",
+				"name" : "(*)动画轨迹（未全部实现）",
 				"link" : "animation.html"
 			},{
-				"name" : "动态迁徙图",
+				"name" : "(*)动态迁徙图",
 				"link" : "geoLineLayer.html"
 			}
 		]
