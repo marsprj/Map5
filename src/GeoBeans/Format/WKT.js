7f0f11648c10979b5e3792989a299b05aa7c001e
marsprj <marsprj@gmail.com>
2016-09-20 10:38:11 +0800

    创建WKTFormat类，实现WKT字符串和Geometry对象的相互转换。

---
+++ src/GeoBeans/Format/WKT.js
@@ -0,0 +1,20 @@
+GeoBeans.WKTFormat = GeoBeans.Class({
+})
+
+/**
+ * 将WKT字符串转换为Geometry对象
+ * @public
+ * @return {[type]} [description]
+ */
+GeoBeans.WKTFormat.prototype.read = function(wkt){
+
+}
+
+/**
+ * 将Geometry对象转换为WKT字符串
+ * @public
+ * @return {[type]} [description]
+ */
+GeoBeans.WKTFormat.prototype.write = function(geometry){
+
+}
