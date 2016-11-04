45a78d9c7edd86cf506d150292a7e421756ce977
fanzia <caisefan@163.com>
2016-11-04 11:15:40 +0800

    增加动画功能之动态迁徙图层。

--- src/GeoBeans/Layer/ThematicLayer/GeoLineLayer.js
+++ src/GeoBeans/Layer/ThematicLayer/GeoLineLayer.js
@@ -1,8 +1,6 @@
 GeoBeans.Layer.GeoLineLayer = GeoBeans.Class(GeoBeans.Layer,{
 	data : null,
 
-	option : null,
-
 	// 动态点的半径
 	minRadius : 1,
 	maxRadius : 15,
@@ -11,59 +9,72 @@ GeoBeans.Layer.GeoLineLayer = GeoBeans.Class(GeoBeans.Layer,{
 	// 外圈的
 	alpha : 0.5,
 
+	// 贝塞尔曲率
+	curveness : null,
 
-	initialize : function(name,data,option){
+	// 按照秒走的图上距离
+	mapDelta : null,
+
+	// 线样式
+	lineSymbolizer : null,
+
+	// 动态迁徙点的样式
+	pointSymbolizer : null,
+
+	// 拖尾比例
+	trailLength : null,
+
+
+	initialize : function(options){
 		GeoBeans.Layer.prototype.initialize.apply(this, arguments);
-		this.data = data;
-		this.option = option;
 
+		if(isValid(options)){
+			if(isValid(options.name)){
+				this.name = options.name;
+			}
+
+			if(isValid(options.data)){
+				this.data = options.data;
+			}
+
+			if(isValid(options.curveness)){
+				this.curveness = options.curveness;
+			}
+
+			if(isValid(options.mapDelta)){
+				this.mapDelta = options.mapDelta;
+			}
+
+			if(isValid(options.lineSymbolizer)){
+				this.lineSymbolizer = options.lineSymbolizer;
+			}
+
+			if(isValid(options.pointSymbolizer)){
+				this.pointSymbolizer = options.pointSymbolizer;
+			}
+
+			if(isValid(options.trailLength)){
+				this.trailLength = options.trailLength;
+			}
+		}
 		this.calculateLine();
 	},
 
 
 	setMap : function(){
 		GeoBeans.Layer.prototype.setMap.apply(this, arguments);
-		window.layer = this;
-		window.requestNextAnimationFrame(this.animate);
+		this.map.beginAnimate();
 	},
 
-	load : function(){
-		this.drawLine();
-		layer.drawToPoint();
-		this.flag = GeoBeans.Layer.Flag.LOADED;
-	},
-
-
-	animate : function(time){
-		var layer = this.layer;
-		layer.renderer.clearRect();
-		layer.drawLine();
-		layer.drawMovingDot(time);
-		layer.drawToPoint(time);
-		window.requestNextAnimationFrame(layer.animate); 
-	},
-
-	// draw : function(){
-	// 	var width = this.map.width;
-	// 	var height = this.map.height;
-
-	// 	var x = Math.random()*100;
-	// 	var y = Math.random()*90;
-	// 	var center = new GeoBeans.Geometry.Point(x,y);
-	// 	var circle = new GeoBeans.Geometry.Circle(center,5);
-	// 	var symbolizer = new GeoBeans.Symbolizer.PolygonSymbolizer();
-	// 	this.renderer.setSymbolizer(symbolizer);
-	// 	this.renderer.drawGeometry(circle,symbolizer,this.map.transformation);
-	// 	this.map.drawLayersAll();
-	// },
-
+	// 绘制线
 	drawLine : function(){
-		this.renderer.setSymbolizer(this.option.symbolizer);
+		this.renderer.setSymbolizer(this.lineSymbolizer);
 		for(var i = 0; i < this.data.length;++i){
 			var line = this.data[i];
-			this.renderer.drawBezierLine(line.from,line.to,line.control,this.map.getMapViewer());
+			this.renderer.drawBezierLine(line.from,line.to,line.control,this.map.getViewer());
 		}
 	},
+
 	// 计算贝塞尔曲线
 	calculateLine : function(){
 		if(this.data == null){
@@ -71,27 +82,27 @@ GeoBeans.Layer.GeoLineLayer = GeoBeans.Class(GeoBeans.Layer,{
 		}
 		for(var i = 0; i < this.data.length;++i){
 			var line = this.data[i];
-			var point = this.getBezierControlPoint(line.from,line.to,this.option.curveness);
+			var point = this.getBezierControlPoint(line.from,line.to,this.curveness);
 			line.control = point;
-			var distance = this.getDistance(line.from,line.to);
-			var alltime = distance/this.option.mapDelta*1000;
+			var distance = GeoBeans.Utility.getDistance(line.from.x,line.from.y,line.to.x,line.to.y);
+			var alltime = distance/this.mapDelta*1000;
 			// 每条线的运行时间
 			line.alltime = alltime;
 			line.points = [];
 			// 拖尾长度
-			var trailLength = distance*this.option.trailLength;
+			var trailLength = distance*this.trailLength;
 			line.trailLength = trailLength;
 		}
 
 	},
 
-
+	// 获取贝塞尔曲线的控制点
 	getBezierControlPoint : function(from,to,curveness){
 		if(from == null || to == null){
 			return null;
 		}
 		var center = new GeoBeans.Geometry.Point(from.x/2+to.x/2,from.y/2+to.y/2);
-		var distance = this.getDistance(from,to);
+		var distance = GeoBeans.Utility.getDistance(from.x,from.y,to.x,to.y);
 		var k = (from.x-to.x)/(from.y-to.y);
 		var angle = Math.atan(k);
 		angle = Math.PI/2 - angle;
@@ -105,15 +116,9 @@ GeoBeans.Layer.GeoLineLayer = GeoBeans.Class(GeoBeans.Layer,{
 	},
 
 
-	getDistance : function(p1,p2){
-		return Math.sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y));
-	},
-
 	// 绘制移动的点
 	drawMovingDot : function(time){
-		this.renderer.setSymbolizer(this.option.pointSymbolizer);
-		// this.renderer.context.shadowBlur=20;
-		// this.renderer.context.shadowColor = this.option.pointSymbolizer.fill.color.getRgba();
+		this.renderer.setSymbolizer(this.pointSymbolizer);
 		var p0 = null, p1 = null, p2 = null;
 		var alltime = null;
 		for(var i = 0; i< this.data.length;++i){
@@ -137,11 +142,10 @@ GeoBeans.Layer.GeoLineLayer = GeoBeans.Class(GeoBeans.Layer,{
 			var x = (1-t) * (1-t)*p0.x + 2*t*(1-t)*p1.x + t*t*p2.x;
 			var y = (1-t) * (1-t)*p0.y + 2*t*(1-t)*p1.y + t*t*p2.y;
 			var point = new GeoBeans.Geometry.Point(x,y);
-			this.renderer.drawGeometry(point,this.option.pointSymbolizer,this.map.getMapViewer());
+			this.renderer.drawGeometry(point,this.pointSymbolizer,this.map.getViewer());
 			line.points.push(point);
 			this.drawTrail(line,point);
 		}
-		this.map.drawLayersAll();
 	},
 
 	// 绘制拖尾，当前点
@@ -151,7 +155,7 @@ GeoBeans.Layer.GeoLineLayer = GeoBeans.Class(GeoBeans.Layer,{
 		var drawPoints = [];
 		for(var i = points.length - 1; i >=0;--i){
 			var prePoint = points[i];
-			var distance = this.getDistance(point,prePoint);
+			var distance = GeoBeans.Utility.getDistance(point.x,point.y,prePoint.x,prePoint.y);
 			if(distance < trailLength){
 				drawPoints.push(prePoint);
 			}
@@ -160,57 +164,56 @@ GeoBeans.Layer.GeoLineLayer = GeoBeans.Class(GeoBeans.Layer,{
 			return;
 		}
 		
-		// gradient.addColorStop(0.00,"red");
-  //       gradient.addColorStop(1.00,"red");        
-        var radius = this.option.pointSymbolizer.size;
+       
+        var radius = this.pointSymbolizer.size;
         this.renderer.context.lineWidth=radius*2;
-        var spt = this.map.getMapViewer().toScreenPoint(point.x,point.y);
+        var spt = this.map.getViewer().toScreenPoint(point.x,point.y);
         this.renderer.context.beginPath();
         this.renderer.context.moveTo(spt.x,spt.y);
 		for(var i = 0; i < drawPoints.length;++i){
-			var pt = this.map.getMapViewer().toScreenPoint(drawPoints[i].x,drawPoints[i].y);
+			var pt = this.map.getViewer().toScreenPoint(drawPoints[i].x,drawPoints[i].y);
 			this.renderer.context.lineTo(pt.x,pt.y);
 		}
 		var endPoint = drawPoints[drawPoints.length - 1];
-		var sendPoint = this.map.getMapViewer().toScreenPoint(endPoint.x,endPoint.y);
+		var sendPoint = this.map.getViewer().toScreenPoint(endPoint.x,endPoint.y);
 		var gradient = this.renderer.context.createLinearGradient(spt.x,spt.y,sendPoint.x,sendPoint.y);
-		gradient.addColorStop(0.00,this.option.pointSymbolizer.fill.color.getRgba());
+		gradient.addColorStop(0.00,this.pointSymbolizer.fill.color.getRgba());
         gradient.addColorStop(1.00,"rgba(255,255,255,0)");
         this.renderer.context.strokeStyle = gradient;
 		this.renderer.context.stroke();
 	},
+
 
 	// 绘制终点
 	drawToPoint : function(time){
 		this.calculateRadius();
 
 		// 和线一个颜色
-		var color = this.option.symbolizer.stroke.color;
+		var color = this.lineSymbolizer.stroke.color;
 		var rgb = color.getRgb();
+		var innerLineSymbolizer = this.lineSymbolizer.clone();
 
-
-		var outerColor = rgb.replace("rgb", "rgba").replace(")", "") + ",";
-		outerColor += this.alpha + ")";
-		
-		var context = this.renderer.context;
+		var outerLineSymbolizer = this.lineSymbolizer.clone();
+		outerLineSymbolizer.stroke.color.setOpacity(this.alpha);
 
 		for(var i = 0; i < this.data.length;++i){
 			var line = this.data[i];
 			var to = line.to;
 
-			context.strokeStyle = rgb;
-			var toPoint = this.map.getMapViewer().toScreenPoint(to.x,to.y);
-			context.beginPath();
-			context.arc(toPoint.x,toPoint.y,this.innerRadius,0, 2 * Math.PI);
-			context.stroke();
-			context.closePath();	
+			var toPoint = this.map.getViewer().toScreenPoint(to.x,to.y);
+			var p_inner = new GeoBeans.Geometry.Point(toPoint.x + this.innerRadius, toPoint.y + this.innerRadius);
+			var p_inner_m = this.map.getViewer().toMapPoint(p_inner.x,p_inner.y);
+			var innerRadius = GeoBeans.Utility.getDistance(to.x,to.y,p_inner_m.x,p_inner_m.y);
+			var circleInner = new GeoBeans.Geometry.Circle(to,innerRadius);
+			this.renderer.setSymbolizer(innerLineSymbolizer);
+			this.renderer.drawGeometry(circleInner,innerLineSymbolizer,this.map.getViewer());
 
-			context.strokeStyle = outerColor;
-			var toPoint = this.map.getMapViewer().toScreenPoint(to.x,to.y);
-			context.beginPath();
-			context.arc(toPoint.x,toPoint.y,this.outerRadius,0, 2 * Math.PI);
-			context.stroke();	
-			context.closePath();			
+			var p_outer = new GeoBeans.Geometry.Point(toPoint.x + this.outerRadius, toPoint.y + this.outerRadius);
+			var p_outer_m = this.map.getViewer().toMapPoint(p_outer.x,p_outer.y);
+			var outerRadius = GeoBeans.Utility.getDistance(to.x,to.y,p_outer_m.x,p_outer_m.y);
+			var circleOuter = new GeoBeans.Geometry.Circle(to,outerRadius);
+			this.renderer.setSymbolizer(outerLineSymbolizer);
+			this.renderer.drawGeometry(circleOuter,outerLineSymbolizer,this.map.getViewer());	
 
 		}
 	},
@@ -241,3 +244,33 @@ GeoBeans.Layer.GeoLineLayer = GeoBeans.Class(GeoBeans.Layer,{
 	},
 
 });
+
+/**
+ * 是否是动画图层
+ * @private
+ * @return {Boolean} 
+ */
+GeoBeans.Layer.GeoLineLayer.prototype.isAnimation = function(){
+	return true;
+};
+
+/**
+ * 绘制
+ * @private
+ * @param  {int} time 时间
+ */
+GeoBeans.Layer.GeoLineLayer.prototype.draw = function(time){
+	if(!isValid(time)){
+		return;
+	}
+
+	if(!this.isVisible()){
+		this.clear();
+		return;
+	}
+		
+	this.clear();
+	this.drawLine();
+	this.drawMovingDot(time);
+	this.drawToPoint(time);
+}
